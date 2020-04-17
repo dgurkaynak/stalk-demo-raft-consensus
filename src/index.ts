2c426dd1cb9866f51fa2fe617e2b1c40ff6dc286
Deniz Gurkaynak <dgurkaynak@gmail.com>
2020-04-17 13:25:56 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+async function main() {
+  console.log('hello bitcheeees!');
+}
+
+main().catch((err) => console.error(err));
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  target: 'web',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      hash: true
+    })
+  ],
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
